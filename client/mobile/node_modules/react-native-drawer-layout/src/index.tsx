85c5b9f4401298c20fe4f0f759a0306a39ff74e8
Phạm Hữu Đức <82891699+Huuduc2707@users.noreply.github.com>
2023-11-27 01:04:41 +0700

    Delete client/mobile/node_modules directory

--- client/mobile/node_modules/react-native-drawer-layout/src/index.tsx
+++
@@ -1,4 +0,0 @@
-export { DrawerGestureContext } from './utils/DrawerGestureContext';
-export { DrawerProgressContext } from './utils/DrawerProgressContext';
-export { useDrawerProgress } from './utils/useDrawerProgress';
-export { Drawer } from './views/Drawer';
